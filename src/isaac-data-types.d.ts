8c42d02c03dd8035d1871a673696f2568ede79ee
sxpydo <shavypeynado@gmail.com>
2024-08-20 13:07:13 +0100

    generated types instead of manually updating

--- src/isaac-data-types.d.ts
+++ src/isaac-data-types.d.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2024-08-12 11:50:50.
+// Generated using typescript-generator version 3.2.1263 on 2024-08-20 12:58:59.
 
 export interface AbstractUserPreferenceManager {
 }
